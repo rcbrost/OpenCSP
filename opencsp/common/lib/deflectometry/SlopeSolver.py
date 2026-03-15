d4a7a2c2985a43164f4e151ad50d12ec7b0a446c
rcbrost <rcbrost@sandia.gov>
2026-03-15 12:56:23 -0600

    Checkpoint before exploring new solver algorithm.  Lots of scaffolding.

--- opencsp/common/lib/deflectometry/SlopeSolver.py
+++ opencsp/common/lib/deflectometry/SlopeSolver.py
@@ -97,11 +97,11 @@ class SlopeSolver:
         trans_align = TransformXYZ.from_zero_zero()
 
         # &&&& DELETE-SCAFFOLDING -- TEMPORARY PASS-THROUGH HACK
-        # for idx1 in range(4):
-        for idx1 in range(1):  # &&&& DELETE-SCAFFOLDING -- TEMPORARY PASS-THROUGH HACK
+        for idx1 in range(4):
+            # for idx1 in range(1):  # &&&& DELETE-SCAFFOLDING -- TEMPORARY PASS-THROUGH HACK
             # &&&& DELETE-SCAFFOLDING -- TEMPORARY PASS-THROUGH HACK
-            # for idx2 in range(3):
-            for idx2 in range(1):  # &&&& DELETE-SCAFFOLDING -- TEMPORARY PASS-THROUGH HACK
+            for idx2 in range(3):
+                # for idx2 in range(1):  # &&&& DELETE-SCAFFOLDING -- TEMPORARY PASS-THROUGH HACK
                 # Calculate surface intersection points
                 self.surface.calculate_surface_intersect_points()
 
@@ -124,12 +124,12 @@ class SlopeSolver:
                         stacklevel=2,
                     )
 
-                # Plot debug plot
-                if self.debug.debug_active:
-                    self._plot_debug_plots("Before Slope Fit", idx1, idx2)
-                    self._plot_debug_plots("Before Slope Fit", idx1, idx2, az_el_roll_deg=(0, 0, 0))
-                    self._plot_debug_plots("Before Slope Fit", idx1, idx2, az_el_roll_deg=(-90, 0, 0))
-                    self._plot_debug_plots("Before Slope Fit", idx1, idx2, az_el_roll_deg=(0, 90, 90))
+                # # Plot debug plot
+                # if self.debug.debug_active:
+                #     self._plot_debug_plots("Before Slope Fit", idx1, idx2)
+                #     # self._plot_debug_plots("Before Slope Fit", idx1, idx2, az_el_roll_deg=(0, 0, 0))
+                #     # self._plot_debug_plots("Before Slope Fit", idx1, idx2, az_el_roll_deg=(-90, 0, 0))
+                #     # self._plot_debug_plots("Before Slope Fit", idx1, idx2, az_el_roll_deg=(0, 90, 90))
 
                 # Update slope fit
                 self.surface.fit_slopes()
@@ -141,43 +141,42 @@ class SlopeSolver:
                     self._plot_debug_plots("After Slope Fit", idx1, idx2, az_el_roll_deg=(-90, 0, 0))
                     self._plot_debug_plots("After Slope Fit", idx1, idx2, az_el_roll_deg=(0, 90, 90))
 
-            # # &&&& DELETE-SCAFFOLDING -- TEMPORARY PASS-THROUGH HACK
-            # # Calculate measure point intersection point with existing fitting function
-            # v_meas_pts_surf_int_optic = self.surface.intersect(u_measure_pixel_pointing_optic, v_optic_cam_optic)
+            # Calculate measure point intersection point with existing fitting function
+            v_meas_pts_surf_int_optic = self.surface.intersect(u_measure_pixel_pointing_optic, v_optic_cam_optic)
 
-            # # Calculate design normal at alignment point
-            # n_design = self.surface.normal_design_at_align_point()
+            # Calculate design normal at alignment point
+            n_design = self.surface.normal_design_at_align_point()
 
-            # # Calculate measured normal at alignment point
-            # n_meas = self.surface.normal_fit_at_align_point()
+            # Calculate measured normal at alignment point
+            n_meas = self.surface.normal_fit_at_align_point()
 
-            # # Calculate the rotation needed to align the normal vectors
-            # r_align_step = n_meas.align_to(n_design)
+            # Calculate the rotation needed to align the normal vectors
+            r_align_step = n_meas.align_to(n_design)
 
-            # # Rotate all points about alignment point
-            # self.surface.rotate_all(r_align_step)
+            # Rotate all points about alignment point
+            self.surface.rotate_all(r_align_step)
 
-            # # # Plot debug plot
-            # # if self.debug.debug_active:
-            # #     self._plot_debug_plots("After Rotate All", idx1, idx2)
-            # #     # self._plot_debug_plots("After Rotate All", idx1, idx2, az_el_roll_deg=(0, 0, 0))
-            # #     # self._plot_debug_plots("After Rotate All", idx1, idx2, az_el_roll_deg=(-90, 0, 0))
-            # #     # self._plot_debug_plots("After Rotate All", idx1, idx2, az_el_roll_deg=(0, 90, 90))
+            # # Plot debug plot
+            # if self.debug.debug_active:
+            #     self._plot_debug_plots("After Rotate All", idx1, idx2)
+            #     self._plot_debug_plots("After Rotate All", idx1, idx2, az_el_roll_deg=(0, 0, 0))
+            #     self._plot_debug_plots("After Rotate All", idx1, idx2, az_el_roll_deg=(-90, 0, 0))
+            #     self._plot_debug_plots("After Rotate All", idx1, idx2, az_el_roll_deg=(0, 90, 90))
 
-            # # Calculate scale so that align-point to screen matches measurement
-            # args = (
-            #     dist_optic_screen,
-            #     v_align_point_optic,
-            #     v_optic_cam_optic,
-            #     v_optic_screen_optic,
-            #     v_meas_pts_surf_int_optic,
-            # )
-            # out = minimize(sf2.dist_optic_screen_error, np.array([1.0]), args=args)
-            # scale = out.x[0]
-            # v_align_optic_step = (v_optic_cam_optic - v_align_point_optic) * (scale - 1)
+            # Calculate scale so that align-point to screen matches measurement
+            args = (
+                dist_optic_screen,
+                v_align_point_optic,
+                v_optic_cam_optic,
+                v_optic_screen_optic,
+                v_meas_pts_surf_int_optic,
+            )
+            out = minimize(sf2.dist_optic_screen_error, np.array([1.0]), args=args)
+            scale = out.x[0]
+            v_align_optic_step = (v_optic_cam_optic - v_align_point_optic) * (scale - 1)
 
-            # # Shift all points along align-point to camera axis
-            # self.surface.shift_all(v_align_optic_step)
+            # Shift all points along align-point to camera axis
+            self.surface.shift_all(v_align_optic_step)
 
             # # Plot debug plot
             # if self.debug.debug_active:
@@ -186,16 +185,16 @@ class SlopeSolver:
             #     # self._plot_debug_plots("After Shift All", idx1, idx2, az_el_roll_deg=(-90, 0, 0))
             #     # self._plot_debug_plots("After Shift All", idx1, idx2, az_el_roll_deg=(0, 90, 90))
 
-            # # Calculate alignment transform
-            # trans_step = TransformXYZ.from_R_V(r_align_step, v_align_optic_step)
-            # trans_align = trans_step * trans_align
+            # Calculate alignment transform
+            trans_step = TransformXYZ.from_R_V(r_align_step, v_align_optic_step)
+            trans_align = trans_step * trans_align
 
         # Store alignment parameters
         self._data.surf_coefs_facet = self.surface.surf_coefs
         self._data.slope_coefs_facet = self.surface.slope_coefs
         # &&&& DELETE-SCAFFOLDING -- TEMPORARY PASS-THROUGH HACK
-        # self._data.trans_alignment = trans_align
-        self._data.trans_alignment = TransformXYZ.identity()  # &&&& DELETE-SCAFFOLDING -- TEMPORARY PASS-THROUGH HACK
+        self._data.trans_alignment = trans_align
+        # self._data.trans_alignment = TransformXYZ.identity()  # &&&& DELETE-SCAFFOLDING -- TEMPORARY PASS-THROUGH HACK
 
     def solve_slopes(self) -> None:
         """
